06094cfff9164965aa9d0d5c21caad7f11b2c254
qwerty16482dj-code <qwerty16482dj@gmail.com>
2025-12-18 13:40:27 +0200

    Prepare server for deploy

--- the-schematics/client/src/lib/supabase.js
+++ the-schematics/client/src/lib/supabase.js
@@ -1,10 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-client'
 
-// Vite требует префикс VITE_ для переменных среды
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
+  // Эта строка как раз и выдает вашу ошибку в консоль
   throw new Error('Missing Supabase Environment Variables')
 }
 
